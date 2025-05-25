546bdf2b34365937ed5e8c36dda3fc9adebcc094
s22stuledua <s22stuledua@venta.lv>
2025-05-25 18:42:10 +0300

    cosmetic changes

--- src/main/java/vea/controller/ClassroomController.java
+++ src/main/java/vea/controller/ClassroomController.java
@@ -1,7 +1,6 @@
 package vea.controller;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
